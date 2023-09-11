75b70769b7932936d6fe597a420630ca800ccb4b
Kelly Santos <105869919+AnakellySantosMascarenhas@users.noreply.github.com>
2023-09-11 18:02:08 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html> <html lang="pt-br">
+     <head> <meta charset="UTF-8"> 
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+     <title>Juros Simples</title> 
+    
+    </head> 
+    <body> 
+        <form action="resultado.php" method="post"> Calcule o Juros Simples : 
+            <br> 
+            Digite o capital: <input type="Textarea" name="capital"><br>
+            Digite a taxa: <input type="Textarea" name="taxa"><br> 
+            Digite o tempo:  <input type="Textarea" name="tempo"><br>
+             <input type="submit"> 
+        </form> 
+    </body> 
+    </html>
